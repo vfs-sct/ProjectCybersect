e57c0b42b355afad1a74603040be7d42b6a0ce4c
Yuya Yoshino <yuyayoshino7@gmail.com>
2020-05-19 10:20:54 -0700

    Options data

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,6 +6,12 @@ using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
+    public OptionsSetting settings;
+
+    public float MasterVolume = 1f;
+    public float MusicVolume = 0.5f;
+    public float SFXVolume = 0.5f;
+
     public void PlayGame()
     {
 
