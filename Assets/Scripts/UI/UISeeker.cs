dbe52d35e0fb5a694f238c7c176267d368a4f1e3
Keegan Beaulieu <keegan.g.beaulieu@gmail.com>
2020-06-08 14:58:54 -0600

    ui seeker added

---
+++ Assets/Scripts/UI/UISeeker.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UISeeker : MonoBehaviour
+{
+    public Sprite unlocked;
+    public Sprite locked;
+    public Sprite outOfRange;
+
+    private Grapple grapple;
+    private Image seekerImage;
+
+    private void Start()
+    {
+        grapple = GameObject.Find("player").GetComponent<Grapple>();
+        seekerImage = GetComponent<Image>();
+    }
+
+    GrappleCursorState lastCursorState;
+    private void LateUpdate()
+    {
+        if (lastCursorState != grapple.cursorState)
+        {
+            switch (grapple.cursorState)
+            {
+                case GrappleCursorState.LOCKED:
+                    seekerImage.image = locked.texture;
+                    break;
+                case GrappleCursorState.UNLOCKED:
+                    seekerImage.image = unlocked.texture;
+                    break;
+                case GrappleCursorState.OUT_OF_RANGE:
+                    seekerImage.image = outOfRange.texture;
+                    break;
+            }
+        }
+
+        lastCursorState = grapple.cursorState;
+    }
+}
