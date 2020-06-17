2789429da90245f4900a932c4a1c5e886170d81a
Yuya Yoshino <yuyayoshino7@gmail.com>
2020-06-16 20:13:24 -0700

    minor changes

--- Assets/Editor/RotateMesh.cs
+++ Assets/Editor/RotateMesh.cs
@@ -1,3 +1,5 @@
+//https://answers.unity.com/questions/561786/how-to-export-obj-from-editor-with-rescaled-mesh.html?_ga=2.22974716.637888141.1592259629-1152228443.1579129359
+
 using UnityEngine;
 using UnityEditor;
 using System.Collections.Generic;
@@ -7,17 +9,20 @@ public class RotateMesh : EditorWindow {
     private string error = "";
     
     [MenuItem("Window/Rotate Mesh %#r")]
-    public static void ShowWindow() {
+    public static void Initialize() 
+    {
         EditorWindow.GetWindow(typeof(RotateMesh));
     }
 
-    void OnGUI() {
+    void OnGUI() 
+    {
         Transform curr = UnityEditor.Selection.activeTransform;
         GUILayout.Label ("Creates a clone of the game object with a rotated mesh\n" + 
             "so that the rotation will be (0,0,0) and the scale will\nbe (1,1,1).");
         GUILayout.Space(20);
 
-        if (GUILayout.Button ("Rotate Mesh")) {
+        if (GUILayout.Button ("Rotate Mesh")) 
+        {
             error = "";
             RotateTheMesh();
         }
@@ -26,39 +31,45 @@ public class RotateMesh : EditorWindow {
         GUILayout.Label(error);
     }
     
-    void RotateTheMesh() {
+    void RotateTheMesh() 
+    {
         List<Transform> children = new List<Transform>();
-        Transform curr = UnityEditor.Selection.activeTransform;
+        Transform current = UnityEditor.Selection.activeTransform;
 
         MeshFilter mf;
-        if (curr == null) {
+        if (current == null) 
+        {
             error = "No appropriate object selected.";
             Debug.Log (error);    
             return;
         }
         
-        if (curr.localScale.x < 0.0 || curr.localScale.y < 0.0f || curr.localScale.z < 0.0f) {
+        if (current.localScale.x < 0.0 || current.localScale.y < 0.0f || current.localScale.z < 0.0f) 
+        {
             error = "Cannot process game object with negative scale values.";
             Debug.Log (error);
             return;
         }
         
-        mf = curr.GetComponent<MeshFilter>();
-        if (mf == null || mf.sharedMesh == null) {
+        mf = current.GetComponent<MeshFilter>();
+        if (mf == null || mf.sharedMesh == null) 
+        {
             error = "No mesh on the selected object";
             Debug.Log (error);
             return;
         }
         
         // Create the duplicate game object
-        GameObject go = Instantiate (curr.gameObject) as GameObject;
+        GameObject go = Instantiate (current.gameObject) as GameObject;
         mf = go.GetComponent<MeshFilter>();
         mf.sharedMesh = Instantiate (mf.sharedMesh) as Mesh;
-        curr = go.transform;
+        current = go.transform;
         
         // Disconnect any child objects and same them for later
-        foreach (Transform child in curr) {
-            if (child != curr) {
+        foreach (Transform child in current) 
+        {
+            if (child != current) 
+            {
                 children.Add (child);
                 child.parent = null;
             }
@@ -66,50 +77,57 @@ public class RotateMesh : EditorWindow {
         
         // Rotate and scale the mesh
         Vector3[] vertices = mf.sharedMesh.vertices;
-        for (int i = 0; i < vertices.Length; i++) {
-            vertices[i] = curr.TransformPoint(vertices[i]) - curr.position;
+        for (int i = 0; i < vertices.Length; i++) 
+        {
+            vertices[i] = current.TransformPoint(vertices[i]) - current.position;
         }
         mf.sharedMesh.vertices = vertices;
 
         
         // Fix the normals
         Vector3[] normals = mf.sharedMesh.normals;
-        if (normals != null) {
+        if (normals != null) 
+        {
             for (int i = 0; i < normals.Length; i++)
-                normals[i] = curr.rotation * normals[i];
+                normals[i] = current.rotation * normals[i];
         }
         mf.sharedMesh.normals = normals;
         mf.sharedMesh.RecalculateBounds();
         
-        curr.transform.rotation = Quaternion.identity;
-        curr.localScale = new Vector3(1,1,1);
+        current.transform.rotation = Quaternion.identity;
+        current.localScale = new Vector3(1,1,1);
     
         // Restore the children
-        foreach (Transform child in children) {
-            child.parent = curr;
+        foreach (Transform child in children) 
+        {
+            child.parent = current;
         }
         
         // Set selection to new game object
-        UnityEditor.Selection.activeObject = curr.gameObject;
+        UnityEditor.Selection.activeObject = current.gameObject;
 
         //--- Do a rudamentary fixup of mesh, box, and sphere colliders----
-        MeshCollider mc = curr.GetComponent<MeshCollider>();
-        if (mc != null) {
+        MeshCollider mc = current.GetComponent<MeshCollider>();
+        if (mc != null) 
+        {
             mc.sharedMesh = mf.sharedMesh;
         }
         
-        BoxCollider bc = curr.GetComponent<BoxCollider>();
-        if (bc != null) {
+        BoxCollider bc = current.GetComponent<BoxCollider>();
+        if (bc != null) 
+        {
             DestroyImmediate(bc);
-            curr.gameObject.AddComponent<BoxCollider>();
+            current.gameObject.AddComponent<BoxCollider>();
         }
-        SphereCollider sc = curr.GetComponent<SphereCollider>();
-        if (sc != null) {
+        SphereCollider sc = current.GetComponent<SphereCollider>();
+        if (sc != null) 
+        {
             DestroyImmediate(sc);
-            curr.gameObject.AddComponent<SphereCollider>();
+            current.gameObject.AddComponent<SphereCollider>();
         }
         
-        if (curr.GetComponent<Collider>()) {
+        if (current.GetComponent<Collider>()) 
+        {
             error = "Be sure to verify size of collider.";
         }
     }
