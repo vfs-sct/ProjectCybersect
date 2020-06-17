294fef68f04516d2ec3f3e96734e14955999a890
Yuya Yoshino <yuyayoshino7@gmail.com>
2020-06-16 18:23:50 -0700

    decal removal

---
+++ Assets/Scripts/Weapon/RemoveDecal.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) 2020 by Yuya Yoshino
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoveDecal : MonoBehaviour
+{
+    private void OnCollisionExit(Collision other)
+    {
+        Destroy(gameObject);
+    }
+}
